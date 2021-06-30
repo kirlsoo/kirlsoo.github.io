3cac5c225ca09429b4e67ea65114f4eda71d7b33
Kristen <kirlme.atschool@gmail.com>
2021-06-30 20:23:31 +0800

    Created react app for kirl-portfolio

---
+++ kirl-portfolio/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi welcome to my website.However, it's still under development.
+        </p>
+
+        <p>
+          Come back next time! :)
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
